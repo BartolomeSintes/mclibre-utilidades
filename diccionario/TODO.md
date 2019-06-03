3837511b7d287109f615026cf7f9edea500fea5a
Bartolomé Sintes <bartolome.sintes@gmail.com>
2019-06-03 16:55:45 +0200

    Diccionario: mejoro análisis y ordeno diccionarios

--- diccionario/TODO.md
+++ diccionario/TODO.md
@@ -1,35 +1,16 @@
-﻿# UTILIDADES
+# DICICONARIO
 
-Quiero hacer una serie de programas para los apuntes. Creo que es mejor hacer programa especializados más que uno general
+Esta aplicación genera la lista de palabras que hay en los ficheros de un directorio. Lo quiero utilizar básicamente como corrector ortográfico pero también para revisar las clases utilizadas
 
-
-# DICCIONARIO
-
-Esta aplicación genera la lista de palabras que hay en los ficheros de un directorio. Lo quiero utilizar básicamente como corrector ortográfico.
+# IDeas para otras utilidades
+* 2019-06-01. Analizar etiquetas: atributos utilizados y orden de los atributos
 
 ## Cosas por hacer
 * 2019-05-31. Podría contar el número de ficheros a revisar y su tamaño y que vaya sacando cuántos lleva (en porcentaje) y cuánto tiempo lleva
-* 2019-06-01. Primero tendría que hacerlo para los html y después los css y svg (htmlParser se cuelga con algunos ficheros svg, js, etc.)
-* 2019-06-01. Que no trate bloques comentados
-* 2019-06-01. Podría hacer una pasada quitando todas las etiquetas y comentarios (con /* o con &lt;!--) y otra pasada mirando los atributos title
-* 2019-06-01. Que ordene el diccionario. Tendría que hacer una forma de interrumpir el análisis
+* 2019-06-01. Que no trate bloques comentado (con /* o con &lt;!--)
+* 2019-06-03. Otro análisis para hacer sería ver dónse utilizan mayúsculas (para corregir dos myúsculas iniciales, por ejemplo).
+* 2019-06-03. Falta que ordene el diccionario otros.
+* 2019-06-03. Al revisar diccionarios que quite repeticiones.
+* 2019-06-03. No tengo claro que la expresión regular que he puesto para eliminar bloques script no falle si hay un &lt; dentro del bloque.
 
-
-# ENLACES ROTOS
-
-Aplicación que cree una página con todos los enlaces para chequearlos (o con Xenu o que sea la propia plicación)
-
-
-# ANALIZA CSS
-
-Aplicación para obtener todas las clases e ids de los apuntes y compararlos con la hoja de estilo. También podría comprobar el orden de los atributos.
-
-
-# GENERADOR DOCUMENTACIÓN
-
-Aplicación para generar mclibre-documentacion. A partir del programa que tengo para generar cds de cdlibre no debería ser muy complicado.
-
-
-# Ideas para otras utilidades
-* 2019-06-01. Analizar etiquetas: atributos utilizados y orden de los atributos
 
--- diccionario/diccionario.py
+++ diccionario/diccionario.py
@@ -1,5 +1,8 @@
 import pathlib, html.parser, re
 
+MINUSCULAS = True
+MAYUSCULAS = False
+
 DICT_ESPANOL = "diccionario_espanol.txt"
 DICT_INGLES = "diccionario_ingles.txt"
 DICT_HTML = "diccionario_html.txt"
@@ -12,16 +15,16 @@ DICT_PHP = "diccionario.txt"
 DICT_OTROS = "diccionario_otros.txt"
 
 DICT_LIST = [
-    [DICT_ESPANOL, "español"],
-    [DICT_INGLES, "inglés"],
-    [DICT_OTROS, "otros"],
-    [DICT_HTML, "html"],
-    [DICT_CSS, "css"],
-    [DICT_SVG, "svg"],
-    [DICT_MIS_CSS, "mis-css"],
-    [DICT_JAVASCRIPT, "js"],
-    [DICT_PYTHON, "py"],
-    [DICT_PHP, "php"],
+    [DICT_ESPANOL, "español", MINUSCULAS],
+    [DICT_INGLES, "inglés", MINUSCULAS],
+    [DICT_OTROS, "otros", MAYUSCULAS],
+    [DICT_HTML, "html", MINUSCULAS],
+    [DICT_CSS, "css", MINUSCULAS],
+    [DICT_SVG, "svg", MINUSCULAS],
+    [DICT_MIS_CSS, "mis-css", MINUSCULAS],
+    [DICT_JAVASCRIPT, "js", MINUSCULAS],
+    [DICT_PYTHON, "py", MINUSCULAS],
+    [DICT_PHP, "php", MINUSCULAS],
 ]
 
 ORIGEN_HTML = "C:\\Users\\BLJ\\Documents\\_MCLibre.org\\Actual\\consultar\\htmlcss"
@@ -90,6 +93,43 @@ def estadisticas():
     print()
 
 
+def ordena_diccionarios():
+    # Carga diccionarios
+    for i in DICT_LIST:
+        if i[2] == MINUSCULAS:
+            dicts = []
+            # print(f"Carga {i[1]}")
+            if pathlib.Path(i[0]).exists():
+                with open(i[0], "r", encoding="utf-8") as fichero:
+                    palabras = fichero.read().split()
+                dicts += palabras
+                # print(f"El diccionario contine {len(dicts)}.")
+                dicts.sort(reverse=True)
+                for j in range(len(dicts)-1, -1, -1):
+                    if dicts[j][0].lower() != dicts[j][0]:
+                        busca = dicts[j][0].lower() + dicts[j][1:]
+                        k = j
+                        while k > 0:
+                            # print(f"{k} ", end="")
+                            if dicts[k] == busca:
+                                # print(f"Repetida: {dicts[j]} {dicts[k]}")
+                                del dicts[j]
+                                k = 0
+                            k -= 1
+                # print(f"El diccionario contine {len(dicts)}.")
+                for j in range(len(dicts)):
+                    if len(dicts[j]) > 1 and dicts[j][0].lower() != dicts[j][0] and dicts[j][1].lower() == dicts[j][1]:
+                        dicts[j] = dicts[j][0].lower() + dicts[j][1:]
+                        # print(f"Cambia {dicts[j]}")
+                    if len(dicts[j]) == 1 and dicts[j][0].lower() != dicts[j][0]:
+                        dicts[j] = dicts[j][0].lower()
+                        # print(f"Cambia {dicts[j]}")
+                dicts.sort()
+                # print(f"Diccionarios: {dicts}")
+            with open(i[0], "w", encoding="utf-8") as fichero_dic:
+                for palabra in dicts:
+                    print(f"{palabra} ", file=fichero_dic)
+
 class MyHTMLParser(html.parser.HTMLParser):
     lista_urls = []
 
@@ -128,58 +168,122 @@ def main():
             with open(i[0], "r", encoding="utf-8") as fichero:
                 palabras = fichero.read().split()
             dicts += palabras
+    dicts_excepciones = []
+    for i in DICT_LIST:
+        if i[2] == MAYUSCULAS:
+            if pathlib.Path(i[0]).exists():
+                with open(i[0], "r", encoding="utf-8") as fichero:
+                    palabras = fichero.read().split()
+                dicts_excepciones += palabras
     # print(f"Diccionarios: {dicts}")
 
     print("Análisis")
     for extension in EXTENSIONES:
         contador = 0
+        seguir_incluyendo = True
         for filename in pathlib.Path(ORIGEN_HTML).rglob(f"**/*.{extension}"):
-            contador += 1
-            print()
-            print(filename)
-            urls_para_eliminar = elimina_urls(filename)
-            with open(filename, "r", encoding="utf-8") as fichero:
-                texto = fichero.read()
-                # elimina números con unidades
-                unidades = ["%", "px", "em", "rem"]
-                for i in unidades:
-                    x = re.search(f"[\d\.\d]+{i}", texto)
+            if seguir_incluyendo:
+                contador += 1
+                print()
+                print(filename)
+                urls_para_eliminar = elimina_urls(filename)
+                with open(filename, "r", encoding="utf-8") as fichero:
+                    texto = fichero.read()
+                    # elimina scripts JS
+                    x = re.search(r"<script>[^<]+<\/script>", texto)
                     while x:
                         # print(x.group())
                         texto = texto.replace(x.group(), " ")
-                        x = re.search(f"[\d\.\d]+{i}", texto)
-                # elimina atributos que incluyen urls
-                for i in urls_para_eliminar:
-                    texto = texto.replace(i, " ")
-                # elimina entidades numéricas
-                elimina = [" - ", " -\n", "&lt;", "&gt;"]
-                for i in elimina:
-                    texto = texto.replace(i, " ")
-                # elimina caracteres que no son letras
-                elimina = '<>=""\'/().¿?¡!:,;[]()%'
-                for i in elimina:
-                    texto = texto.replace(i, " ")
-                palabras = texto.split()
-                print(palabras)
-                for palabra in palabras:
-                    if not palabra in dicts and not palabra.isnumeric():
-                        print("0: Saltar - ", end="")
-                        for _ in range(len(DICT_LIST)):
-                            print(f"{_+1}: {DICT_LIST[_][1]} - ", end="")
-                        print("x: SALIR")
-                        print()
-                        print(palabra)
-                        print()
-                        incluir = input("Incluir? ")
-                        if incluir == "x":
-                            exit(0)
-                        elif 0 < int(incluir) < len(DICT_LIST):
-                            with open(
-                                DICT_LIST[int(incluir) - 1][0], "a", encoding="utf-8"
-                            ) as fichero_dic:
-                                print(f"{palabra} ", file=fichero_dic)
-                            dicts += [palabra]
+                        x = re.search(r"<script>[^<]+<\/script>", texto)
+                    # elimina etiquetas
+                    x = re.search(r"<[^>]+>", texto)
+                    while x:
+                        # print(x.group())
+                        texto = texto.replace(x.group(), " ")
+                        x = re.search(r"<[^>]+>", texto)
+                    # elimina comentarios CSS /*  ... */
+                    x = re.search(r"\/\*[^*]+\*\/", texto)
+                    while x:
+                        texto = texto.replace(x.group(), " ")
+                        x = re.search(r"\/\*[^*]+\*\/", texto)
+                    # elimina números con unidades
+                    unidades = ["%", "px", "em", "rem"]
+                    for i in unidades:
+                        x = re.search(r"[\d\.\d]+"+i, texto)
+                        while x:
+                            # print(x.group())
+                            texto = texto.replace(x.group(), " ")
+                            x = re.search(r"[\d\.\d]+"+i, texto)
+                    # elimina códigos de colores
+                    x = re.search(r"#[0-9a-fA-F]{3,6}", texto)
+                    while x:
+                        texto = texto.replace(x.group(), " ")
+                        x = re.search(r"#[0-9a-fA-F]{3,6}", texto)
+                    # elimina urls
+                    for i in urls_para_eliminar:
+                        texto = texto.replace(i, " ")
+                    # elimina entidades numéricas
+                    x = re.search("&amp;[#0-9a-zA-Z]+;", texto)
+                    while x:
+                        texto = texto.replace(x.group(), " ")
+                        x = re.search("&amp;[#0-9a-zA-Z]+;", texto)
+                    x = re.search("&[#0-9a-zA-Z]+;", texto)
+                    while x:
+                        texto = texto.replace(x.group(), " ")
+                        x = re.search("&[#0-9a-zA-Z]+;", texto)
+                    # elimina caracteres que no son letras
+                    elimina = '<>=""\'/().¿?¡!:,;[]()%{}'
+                    for i in elimina:
+                        texto = texto.replace(i, " ")
+                    # elimina otros caracteres
+                    elimina = [" - ", " -\n", "\n- ", " + "]
+                    for i in elimina:
+                        texto = texto.replace(i, " ")
+                    # elimina saltos de línea y espacios
+                    texto = texto.replace("\n", " ")
+                    texto = texto.replace("\r", " ")
+                    x = re.search("  +", texto)
+                    while x:
+                        texto = texto.replace(x.group(), " ")
+                        x = re.search("  +", texto)
 
+                    # print(texto)
+                    palabras = texto.split()
+                    for palabra in palabras:
+                        palabra_original = palabra
+                        if not palabra in dicts_excepciones:
+                            if len(palabra) > 1 and palabra[0].lower() != palabra[0] and palabra[1].lower() == palabra[1]:
+                                palabra = palabra[0].lower() + palabra[1:]
+                                # print(f"Cambia {dicts[j]}")
+                            if len(palabra) == 1 and palabra[0].lower() != palabra[0]:
+                                palabra = palabra.lower()
+
+                        if not palabra in dicts and not palabra.isnumeric() and seguir_incluyendo:
+                            print("0: Saltar - ", end="")
+                            for _ in range(len(DICT_LIST)):
+                                print(f"{_+1}: {DICT_LIST[_][1]} - ", end="")
+                            print("x: SALIR")
+                            print()
+                            print(palabra)
+                            print()
+                            incluir = input("Incluir? ")
+                            if incluir == "x":
+                                seguir_incluyendo = False
+                            elif DICT_LIST[int(incluir) - 1][0] == DICT_OTROS:
+                                with open(
+                                    DICT_LIST[int(incluir) - 1][0], "a", encoding="utf-8"
+                                ) as fichero_dic:
+                                    print(f"{palabra_original} ", file=fichero_dic)
+                                dicts += [palabra_original]
+                                dicts_excepciones += [palabra_original]
+                            elif 0 < int(incluir) < len(DICT_LIST):
+                                with open(
+                                    DICT_LIST[int(incluir) - 1][0], "a", encoding="utf-8"
+                                ) as fichero_dic:
+                                    print(f"{palabra} ", file=fichero_dic)
+                                dicts += [palabra]
+
+    ordena_diccionarios()
 
 if __name__ == "__main__":
     main()
