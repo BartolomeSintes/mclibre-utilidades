d1cc6ba80ce00225bf97102baf807b9d3e393478
Bartolomé Sintes <bartolome.sintes@gmail.com>
2019-06-14 23:55:03 +0200

    Generador doc.: Mejoro ordenación revistas

--- generador-documentacion/TODO.md
+++ generador-documentacion/TODO.md
@@ -4,7 +4,13 @@ Esta aplicación genera el sitio web http://www.mclibre.org/consultar/documentac
 
 
 # Cosas para hacer
-* 2019-06-13. Añadir fecha de inclusión de un registro
-* 2019-06-13. Que compruebe que los ficheros de plantillas en ORIGEN y los nombres de las páginas en PAGINA coinciden
-* 2019-06-23. Decidir cómo poenr fecha a cada página (parece que con las fechas de inclusión de los registros)
-* 2019-06-23. Tamaños de los pdfs salen en MB, hacer que salgan en KB si es inferior a 1 MB
+* 2019-06-13. Que compruebe que los ficheros de plantillas en ORIGEN y los nombres de las páginas en PAGINA coinciden.
+* 2019-06-13. La fecha de creación de cada página la tendría que poner a partir las fechas de creación de los registros).
+* 2019-06-13. Tamaños de los pdfs salen en MB, hacer que salgan en KB si es inferior a 1 MB.
+* 2019-06.14. Añadiir en paginas.json el país de origen de cada revista
+* 2019-06-14. Mirando la fecha de las imágenes de las portadas puedo ver cuándo he añadido cada revista.
+* 2019-06-14. El formato de los documentos lo puedo sacar de la extensión. No hace falta que sea una clave.
+* 2019-06-14. la función ordena sólo funciona si reversed=True, me falta corregir si reversed=False
+* 2019-06-14. No ordena por idioma (por ejemplo, para que ponga antes la versión en inglés y luego en español).
+* 2019-06-14. Añadir un campo para explicaciones adicionales (p.e. Aprender para Educar tiene números posteriores que no puedo descargar y Occam's Razor ya no saca pdfs).
+* 2019-06-14. Odroid magazine tiene dos páginas web (versión inglesa y española) http://magazine.odroid.com/es/
--- generador-documentacion/generador.py
+++ generador-documentacion/generador.py
@@ -6,7 +6,7 @@
 
 # COSAS POR HACER
 
-import json, pathlib, shutil, imagesize
+import json, pathlib, shutil, imagesize, operator
 
 
 PAGINAS = "paginas.json"
@@ -40,26 +40,82 @@ MES = [
 def nombre_plantilla_a_nombre_revista(plantilla):
     for i in paginas_json["paginas"]:
         # print(f"{plantilla} - {i['nombre']}")
-        if plantilla == i["nombre"]:
+        if plantilla == i["pagina"]:
             return i
     return "ERROR"
 
 
-def ordena(lista, clave, reverse=False):
-    # Ordena una lista de diccionarios por valores crecientes de una clave
+def ordena(lista, reverse=False):
+    # Ordena una lista de diccionarios por año > mes > nombre revista > número
+    # La comparación es complicada porque meses y números pueden ser int o str
     cambios = True
     while cambios:
         cambios = False
         for i in range(len(lista) - 1):
             if reverse:
-                if lista[i][clave] < lista[i + 1][clave]:
+                if (
+                    # si el año es menor
+                    lista[i]["año"] < lista[i + 1]["año"]
+                    # si los meses son números compara como números
+                    or (lista[i]["año"] == lista[i + 1]["año"]
+                    and (
+                        isinstance(lista[i]["mes"], int)
+                        and isinstance(lista[i+1]["mes"], int)
+                    )
+                    and lista[i]["mes"] < lista[i + 1]["mes"])
+                    # si los meses no son números compara como cadenas
+                    or (lista[i]["año"] == lista[i + 1]["año"]
+                    and (
+                        not isinstance(lista[i]["mes"], int)
+                        or not isinstance(lista[i+1]["mes"], int)
+                    )
+                    and str(lista[i]["mes"]) < str(lista[i + 1]["mes"]))
+                    # si el nombre de la revista es menor (para recopilaciones)
+                    or (lista[i]["año"] == lista[i + 1]["año"]
+                    and str(lista[i]["mes"]) == str(lista[i + 1]["mes"])
+                    and lista[i]["nombre"] < lista[i + 1]["nombre"])
+                    # si los números de revista son números compara como números
+                    or (lista[i]["año"] == lista[i + 1]["año"]
+                    and str(lista[i]["mes"]) == str(lista[i + 1]["mes"])
+                    and lista[i]["nombre"] == lista[i + 1]["nombre"]and (
+                        isinstance(lista[i]["número"], int)
+                        and isinstance(lista[i+1]["número"], int)
+                    )
+                    and lista[i]["número"] < lista[i + 1]["número"])
+                    # si los números de revista no son números compara como cadenas
+                    or (lista[i]["año"] == lista[i + 1]["año"]
+                    and str(lista[i]["mes"]) == str(lista[i + 1]["mes"])
+                    and lista[i]["nombre"] == lista[i + 1]["nombre"]and (
+                        not isinstance(lista[i]["número"], int)
+                        or not isinstance(lista[i+1]["número"], int)
+                    )
+                    and str(lista[i]["número"]) < str(lista[i + 1]["número"]))
+                ):
                     cambios = True
                     lista[i], lista[i + 1] = lista[i + 1], lista[i]
+                    # print("cambiados")
             else:
-                if lista[i][clave] > lista[i + 1][clave]:
+                if (
+                    lista[i]["año"] > lista[i + 1]["año"]
+                    or lista[i]["año"] == lista[i + 1]["año"]
+                    and str(lista[i]["número"]) > str(lista[i + 1]["número"])
+                ):
                     cambios = True
                     lista[i], lista[i + 1] = lista[i + 1], lista[i]
     return lista
+
+
+def meses(numero):
+    if numero == "":
+        return ""
+    elif str(numero).isnumeric() and 1 <= numero and numero <= 12:
+        return MES[numero]
+    elif numero == "S1":
+        return "Primer semestre"
+    elif numero == "S2":
+        return "Segundo semestre"
+    else:
+        return "ERROR"
 
 
 def crea_bloque_revista_anyos(plantilla):
@@ -68,68 +124,72 @@ def crea_bloque_revista_anyos(plantilla):
     # Obtiene ejemplares
     r = nombre_plantilla_a_nombre_revista(plantilla)
     for i in elementos_json["revistas"]:
-        if i["nombre"] == r["revista"]:
+        if i["nombre"] == r["nombre"] or ("serie" in i) and i["serie"] == r["nombre"]:
             ejemplares += [i]
             if i["año"] not in anyos:
                 anyos += [i["año"]]
     anyos.sort(reverse=True)
-
     # Genera html
-    t = ''
-    t += '<!DOCTYPE html>\n'
+    t = ""
+    t += "<!DOCTYPE html>\n"
     t += '<html lang="es">\n'
-    t += '<head>\n'
+    t += "<head>\n"
     t += '  <meta charset="utf-8" />\n'
-    t += f'  <title>Revista {r["revista"]}. Documentación sobre software libre. Bartolomé Sintes Marco. www.mclibre.org</title>'
-    t += '\n'
+    t += f'  <title>Revista {r["nombre"]}. Documentación sobre software libre. Bartolomé Sintes Marco. www.mclibre.org</title>'
+    t += "\n"
     t += '  <meta name="viewport" content="width=device-width, initial-scale=1.0" />\n'
     t += '  <link rel="stylesheet" type="text/css" href="../varios/documentos.css" title="mclibre" />\n'
     t += '  <link rel="icon" href="../varios/favicon.ico" />\n'
-    t += '</head>\n'
-    t += '\n'
-    t += '<body>\n'
-    t += f'  <h1>Revista {r["revista"]}</h1>'
-    t += '\n'
-    t += '\n'
-    t += '  <nav>\n'
-    t += '    <p>\n'
+    t += "</head>\n"
+    t += "\n"
+    t += "<body>\n"
+    t += f'  <h1>Revista {r["nombre"]}</h1>'
+    t += "\n"
+    t += "\n"
+    t += "  <nav>\n"
+    t += "    <p>\n"
     t += '      <a href="../index.html"><img src="../varios/cc.png" alt="Volver a la página principal" title="Volver a la página principal" height="64" width="64" /></a>\n'
     t += '      <a href="#"><img src="../varios/iconos/icono_arrow_circle_up.svg" alt="Principio de la página" title="Principio de la página" width="36" height="36" /></a>\n'
-    t += '    </p>\n'
-    t += '\n'
-    t += f'    <h2>{r["revista"]}</h2>'
-    t += '\n'
-    t += '\n'
+    t += "    </p>\n"
+    t += "\n"
+    t += f'    <h2>{r["nombre-corto"]}</h2>'
+    t += "\n"
+    t += "\n"
     t += '    <div class="toc">\n'
-    t += '      <ul>\n'
+    t += "      <ul>\n"
     for i in anyos:
         t += f'        <li><a href="#y{i}">{i}</a></li>'
-        t += '\n'
-    t += '      </ul>\n'
-    t += '    </div>\n'
-    t += '  </nav>\n'
-    t += '\n'
-    t += f'  <p>Esta página contiene enlaces a los números publicados de la revista <strong>{r["revista"]}</strong> en '
-    for i in range(len(anyos)-1):
+        t += "\n"
+    t += "      </ul>\n"
+    t += "    </div>\n"
+    t += "  </nav>\n"
+    t += "\n"
+    t += f'  <p>Esta página contiene enlaces a los números publicados de la revista <strong>{r["nombre"]}</strong> en '
+    for i in range(len(anyos) - 1):
         t += f'<a href="#y{anyos[i]}">{anyos[i]}</a> - '
     t += f'<a href="#y{anyos[-1]}">{anyos[-1]}</a>.</p>'
-    t += '\n'
-    t += '\n'
-    t += f'  <p>Página web: <a href="{r["web"]}">{r["revista"]}</a></p>'
-    t += '\n'
-    t += '\n'
+    t += "\n"
+    t += "\n"
+    t += f'    <h2>{r["nombre"]}</h2>'
+    t += "\n"
+    t += "\n"
+    t += f'  <p>Página web: <a href="{r["web"]}">{r["nombre"]}</a></p>'
+    t += "\n"
+    t += "\n"
     for a in anyos:
         ejemplares_year = []
         for i in ejemplares:
             if i["año"] == a:
                 ejemplares_year += [i]
-        ejemplares_year = ordena(ejemplares_year, "número", reverse=True)
+        ejemplares_year = ordena(ejemplares_year, reverse=True)
+        # ejemplares_year = sorted(ejemplares_year, key=operator.attrgetter('año', 'mes'), reverse=True)
+        # no me aclaro con sorted, dice que no hay atributo año
 
         t += f'  <section id="y{a}">'
-        t += '\n'
+        t += "\n"
         t += f"    <h2>{a}</h2>"
-        t += '\n'
-        t += '\n'
+        t += "\n"
+        t += "\n"
         t += '    <div class="miniaturas">\n'
         for i in ejemplares_year:
             width, height = imagesize.get(
@@ -138,22 +198,25 @@ def crea_bloque_revista_anyos(plantilla):
             fichero = pathlib.Path(LOCAL_ARCHIVOS + r["archivos"] + i["fichero"])
             weight = str(round(fichero.stat().st_size / 1024 / 1024, 1)) + " MB"
             formato = fichero.suffix[1:].upper()
-            t += '      <div>\n'
-            t += f'        <p><img alt="Revista {i["nombre"]} nº {i["número"]} - {i["año"]}-{i["mes"]:02d}" src="{r["miniaturas"]}{i["portada"]}" width="{width}" height="{height}" /></p>'
-            t += '\n'
-            t += f'        <p>Número {i["número"]} - {i["año"]} {MES[i["mes"]]}</p>'
-            t += '\n'
+            t += "      <div>\n"
+            if type(i["mes"]) is "<class 'int'>":
+                t += f'        <p><img alt="Revista {i["nombre"]} nº {i["número"]} - {i["año"]}-{i["mes"]:02d}" src="{r["miniaturas"]}{i["portada"]}" width="{width}" height="{height}" /></p>'
+            else:
+                t += f'        <p><img alt="Revista {i["nombre"]} nº {i["número"]} - {i["año"]}-{i["mes"]}" src="{r["miniaturas"]}{i["portada"]}" width="{width}" height="{height}" /></p>'
+            t += "\n"
+            t += f'        <p>Número {i["número"]} - {i["año"]} {meses(i["mes"])}</p>'
+            t += "\n"
             t += f'        <p><a href="{REMOTO_ARCHIVOS +r["archivos"]+i["fichero"]}">Descarga</a> ({formato} {weight} {i["idioma"]})</p>'
-            t += '\n'
-            t += '      </div>\n'
-        t += '    </div>\n'
-        t += '  </section>\n'
-        t += '\n'
+            t += "\n"
+            t += "      </div>\n"
+        t += "    </div>\n"
+        t += "  </section>\n"
+        t += "\n"
     t += '  <address id="ultmod">\n'
-    t += '    Autor: Bartolomé Sintes Marco<br /> Última modificación de esta página: 1 de mayo de 2019\n'
-    t += '  </address>\n'
-    t += '</body>\n'
-    t += '</html>\n'
+    t += "    Autor: Bartolomé Sintes Marco<br /> Última modificación de esta página: 1 de mayo de 2019\n"
+    t += "  </address>\n"
+    t += "</body>\n"
+    t += "</html>\n"
     return t
 
 
@@ -193,7 +256,7 @@ print()
 
 paginas = []
 for i in paginas_json["paginas"]:
-    paginas += [i["nombre"]]
+    paginas += [i["pagina"]]
 # print(paginas)
 
 # Crea ficheros
@@ -201,7 +264,9 @@ print("Creando ficheros de destino")
 for i in pathlib.Path(ORIGEN).rglob(f"*/*"):
     fichero_origen = str(i)
     if i.name not in paginas:
-        print(f"El fichero no existe en la plantilla del sitio: {fichero_origen}")
+        print(
+            f"El fichero no existe en la plantilla del sitio: {fichero_origen} {i.name}"
+        )
     else:
         fichero_destino = fichero_origen.replace(ORIGEN, DESTINO)
         print(fichero_destino)
